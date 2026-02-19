ba27c67bf8bb496081b2de500b583b4b9aee6c73
Trapholo01 <thatorapholo@icloud.com>
2026-02-19 16:28:19 +0200

    feat: Week 2 Day 1 - Terraform files added, gitignore updated

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "af-south-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "taskflow-pro"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["af-south-1a", "af-south-1b"]
+}
